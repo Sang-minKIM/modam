90cbc97cfafeb7b365a35a5b8d7078f0eb4fa6d2
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-04-26 15:33:08 +0900

    Add routers for SPA

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { About, Home } from "./pages";
+import { Join } from "./pages/About/Join";
+import { Login } from "./pages/About/Login";
 
 function App() {
-    return <></>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/" element={<About />}>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/join" element={<Join />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/About/index.ts
@@ -0,0 +1,3 @@
+export * from "./About";
+export * from "./Join";
+export * from "./Login";
